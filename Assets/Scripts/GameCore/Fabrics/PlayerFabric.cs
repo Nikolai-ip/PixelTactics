70eb200b98424d85939622083c633a15a3a52f44
nikolai petruchok <petruchok02@mail.ru>
2024-08-26 16:53:16 +0300

    Add: Player fabric

---
+++ Assets/Scripts/GameCore/Fabrics/PlayerFabric.cs
@@ -0,0 +1,28 @@
+﻿using GameCore.Decks;
+using GameCore.Dumps;
+using GameCore.Fields;
+using GameCore.Hands;
+using GameCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.GameCore.HeroModule;
+
+
+namespace Assets.Scripts.GameCore.Fabrics
+{
+    internal class PlayerFabric
+    {
+        public ServiceLocator GetPlayer(IEnumerable<Hero> Heros)
+        {
+            var player = new ServiceLocator();
+            player.Register<Deck>(new StandardDeck(Heros));
+            player.Register<Hand>(new StandardHand());
+            player.Register<Dump>(new StandardDump());
+            player.Register<GameField>(new GameField(GameConfig.FieldSizeX, GameConfig.FieldSizeY));
+            return player;
+        }
+    }
+}
